474de79b5520aa980daf28ff5a6debf708997788
Amey Ambade <ameyambade@gmail.com>
2024-12-02 21:07:13 -0600

    Add assetprefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/focus',
+  assetPrefix: '/focus/', // This is necessary for the asset prefix to be correct
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
